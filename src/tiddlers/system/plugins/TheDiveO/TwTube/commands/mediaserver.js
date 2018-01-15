0ad3ee10fa3b28c1ec8cbd42d506241c257f4722
Harald Albrecht <hidden@hidden.org>
2018-01-15 19:03:21 +0100

    improves comments in --mediaserver command; updates wiki

--- src/tiddlers/system/plugins/TheDiveO/TwTube/commands/mediaserver.js
+++ src/tiddlers/system/plugins/TheDiveO/TwTube/commands/mediaserver.js
@@ -4,6 +4,9 @@ type: application/javascript
 module-type: command
 
 Serve tiddlers and additionally media(!) resources over HTTP.
+When serving media resources, both single requests ("en bloc")
+as well as range transfers are supported. Supported media types
+are currently those registered with the TW core.
 
 \*/
 (function(){
@@ -46,7 +49,7 @@ var Command = function(params, commander, callback) {
 		if (media === "") {
 			return "media path must not be empty";
 		}
-		$tw.utils.log("Media serving at /" + media + " from ./" + media, "brown/orange");
+		$tw.utils.log("Media serving from ./" + media + " at path /" + media, "brown/orange");
 		return this.inheritedExecute();
 	};
 
@@ -91,11 +94,14 @@ var Command = function(params, commander, callback) {
 
 			var range = request.headers.range;
 			if (range) {
-				// an HTTP client requests only part of a media resource
-				$tw.utils.log("media served piece-wise");
+				// an HTTP client requests only part of a media resource, so we
+				// clamp the requested range to the limits of the requested media
+				// file. We also inform the client of the available full media
+				// range, so it can know in advance what to expect.
 				var parts = range.replace(/bytes=/, "").split("-");
 				var start = parseInt(parts[0], 10);
 				var end = parts[1] ? parseInt(parts[1], 10) : mediasize - 1;
+				$tw.utils.log("media served piece-wise from " + start + " to " + end + "/" + mediasize);
 				response.writeHead(206, {
 					"Content-Range": "bytes " + start + "-" + end + "/" + mediasize,
 					"Accept-Ranges": "bytes",
@@ -104,8 +110,9 @@ var Command = function(params, commander, callback) {
 				});
 				fs.createReadStream(mediapath, {start, end}).pipe(response);
 			} else {
-				// an HTTP client requests a media resource
-				$tw.utils.log("media served in en bloc");
+				// an HTTP client requests a media resource en bloc, so we give
+				// the response the media file stream to send back. Sweet and simple.
+				$tw.utils.log("media served en bloc");
 				response.writeHead(200, {
 					"Content-Length": mediasize,
 					"Content-Type": mediatype
